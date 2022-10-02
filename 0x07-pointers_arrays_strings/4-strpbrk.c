d1698a0d8fd50fe0f84cd99136ae2c6da10ed297
mickeym22 <mickiasmitiku16@gmail.com>
2022-10-02 03:04:23 -0700

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,8 +3,8 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: pointer
  * @accept: pointer
- *
  * Return: a pointer to the byte that matches one of the bytes
+ *
  */
 char *_strpbrk(char *s, char *accept)
 {
