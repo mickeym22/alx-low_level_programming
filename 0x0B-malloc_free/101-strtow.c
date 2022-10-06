295c3e33d29d23fca6953e4cf2b608ab25a8bcb4
mickeym22 <mickiasmitiku16@gmail.com>
2022-10-06 03:09:36 -0700

    6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,7 +9,6 @@ char **strtow(char *str);
  * word_len - Locates the index marking the end of the
  *            first word contained within a string.
  * @str: The string to be searched.
- *
  * Return: The index marking the end of the initial word pointed to by str.
  */
 int word_len(char *str)
