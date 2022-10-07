1297fb845fae25524ab2b1d159a58c5c48236790
mickeym22 <mickiasmitiku16@gmail.com>
2022-10-07 01:19:14 -0700

    0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,7 +6,6 @@
  * @s1: first string to copy
  * @s2: second string to copy
  * @n: number of bytes of s2 to copy
- *
  * Return: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
