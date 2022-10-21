4460139ae60573ac4bde380d85964ea7fbb3c1c4
mickias <mickias>
2022-10-21 11:51:00 -0700

    0.0

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,6 @@
 /**
  * list_len - the number of elements in a linked list_t list.
  * @h:the number of nodes
- *
  * Return: the number of elements in a linked list_t list.
  */
 size_t list_len(const list_t *h)
